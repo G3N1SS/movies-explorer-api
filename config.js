5494ad5b5580ecfbef16c13071d50ae5f64b2a5c
unknown <areg.sarkisyan.99@mail.ru>
2023-12-17 15:31:18 +0300

    default db link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const { DB_ADDRESS } = process.env;
+const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
 const rateLimit = require('express-rate-limit');
